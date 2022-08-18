a62aaf97722baba6c04ba7d8eee6f8e2671c1ec0
Lemon <lisandro.m.yo@gmail.com>
2022-08-18 18:58:07 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navi from'./components/navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         asdsa
-        </p>
+        <Navi/>
         <a
           className="App-link"
           href="https://reactjs.org"
